9e0339421017815705391d9e61e5d108bb650aea
alexcharry <luz.alexia.cruz.charry@gmail.com>
2022-12-15 13:50:53 -0500

    nuevo avance hw2

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -93,33 +93,38 @@ function devolverString(str) {
     // Devuelve el valor de "num" elevado al cuadrado
     // ojo: No es raiz cuadrada!
     // Tu código:
-    
+   return Math.pow(num,2);
   }
   
   function elevarAlCubo(num) {
     // Devuelve el valor de "num" elevado al cubo
     // Tu código:
+    return Math.pow(num,3);
   }
   
   function elevar(num, exponent) {
     // Devuelve el valor de "num" elevado al exponente dado en "exponent"
     // Tu código:
+    return Math.pow(num,exponent);
   }
   
   function redondearNumero(num) {
     // Redondea "num" al entero más próximo y devuélvelo
     // Tu código:
+    return Math.round(num);
   }
   
   function redondearHaciaArriba(num) {
     // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
     // Tu código:
+  return Math.ceil(num);
   }
   
   function numeroRandom() {
     //Generar un número al azar entre 0 y 1 y devolverlo
     //Pista: investigá qué hace el método Math.random()
     // Tu código:
+    return Math.random(0,1);
   }
   
   function esPositivo(numero) {
@@ -128,12 +133,19 @@ function devolverString(str) {
     //Si el número es negativo, devolver ---> "Es negativo"
     //Si el número es 0, devuelve false
     // Tu código:
+    if(numero > 0)
+      return "Es positivo";
+    else if (numero < 0)
+      return "Es negativo";
+    else
+      return false;
   }
   
   function agregarSimboloExclamacion(str) {
     // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
     // Ejemplo: "hello world" pasaría a ser "hello world!"
     // Tu código:
+    
   }
   
   function combinarNombres(nombre, apellido) {
